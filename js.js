08ed087cceaacd3e304496a417ad70268447a0e2
LAPTOP-7UCC4PLB\Julian <juliantornielli@gmail.com>
2020-07-24 09:42:03 -0300

    Subimos version K, nuevo armado de la herramienta.

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+
+
 /*
 Arg:
 -25.99
@@ -7,7 +9,6 @@ Ur:
 -20.99
 -16.03
 -11.45
-weight
 Br:
 -21.99
 -15.40
@@ -18,115 +19,76 @@ Pe:
 -10.50
 */
 
-class Pais{
-    constructor(idPaisSeleccionado, precioFranja1,precioFranja2,precioFranja3){
+class Pais {
+    constructor(idPaisSeleccionado, precioFranja1, precioFranja2, precioFranja3) {
         this.idPaisSeleccionado = idPaisSeleccionado;
         this.precioFranja1 = precioFranja1;
         this.precioFranja2 = precioFranja2;
-        this.precioFranja3 = precioFranja3;    
+        this.precioFranja3 = precioFranja3;
+    }
 }
 
+// Crear objetos Pais
+let Uruguay = new Pais("ur", 20.99, 14.69, 10.50);
+let Argentina = new Pais("ar", 25.99, 18.20, 12.99);
+let Brasil = new Pais("br", 21.99, 15.40, 10.99);
+let Peru = new Pais("pe", 20.99, 14.69, 10.50)
+
+function obtenerPais() {
+    let paisSeleccionado = $("#slcCountrySelector").val();
+    if (paisSeleccionado == "ar") {
+        return Argentina
+    } else if (paisSeleccionado == "ur") {
+        return Uruguay
+    } else if (paisSeleccionado == "br") {
+        return Brasil
+    } else if (paisSeleccionado == "pe") {
+        return Peru
+    }
 }
 
-// Crear objetos Pais y agregarlos al listado listaPaises;
+function obtenerFranja(franja) {
+    if (franja == 1) {
+        return obtenerPais().precioFranja1
+    } else if (franja == 2) {
+        return obtenerPais().precioFranja2
+    } else if (franja == 3) {
+        return obtenerPais().precioFranja3
+    }
+}
 
-let Uruguay = new Pais();
-Uruguay.idPaisSeleccionado =  "ur";
-Uruguay.precioFranja1 = 20.99;
-Uruguay.precioFranja2 = 14.69;
-Uruguay.precioFranja3 = 10.50;
-
-
-let Argentina = new Pais();
-Argentina.idPaisSeleccionado =  "ar";
-Argentina.precioFranja1 = 25.99;
-Argentina.precioFranja2 = 18.20;
-Argentina.precioFranja3 = 12.99;
-
-let Brasil = new Pais();
-Brasil.paisSeleccionado = "br";
-Brasil.precioFranja1 = 21.99;
-Brasil.precioFranja2 = 15.40;
-Brasil.precioFranja3 = 10.99;
-
-function calcularPrecioAbonadoPorCliente(pesoAbonadoCliente,paisSeleccionado){
+function calcularPrecio(abonadoPorCliente) {
     let precioKilo;
-    pesoAbonadoCliente = Number($("#txtPayByClient").val());
-    paisSeleccionado = $("#slcCountrySelector").val();
-    if(paisSeleccionado === "ar"){
-        if(pesoAbonadoCliente > 0 && pesoAbonadoCliente <= 3){
-            precioKilo =  Math.round(pesoAbonadoCliente * Argentina.precioFranja1);
-        }
-        if(pesoAbonadoCliente >= 3.01 && pesoAbonadoCliente <= 5){
-            precioKilo = Math.round(Argentina.precioFranja1 * 3 +  (pesoAbonadoCliente - 3) * Argentina.precioFranja2);
-        }
-        if(pesoAbonadoCliente > 5.01){
-            precioKilo = Math.round(Argentina.precioFranja1 * 3 + Argentina.precioFranja2 * 2 + (pesoAbonadoCliente - 5) * Argentina.precioFranja3);
-        }
-    }
-    if(paisSeleccionado === "ur"){
-        if(pesoAbonadoCliente > 0 && pesoAbonadoCliente <= 3){
-            precioKilo =  Math.round(pesoAbonadoCliente * Uruguay.precioFranja1);
-        }
-        if(pesoAbonadoCliente > 3.01 && pesoAbonadoCliente <= 5){
-            precioKilo = Math.round(Uruguay.precioFranja1 * 3 +   (pesoAbonadoCliente - 3) * Uruguay.precioFranja2);
-        }
-        if(pesoAbonadoCliente > 5.01){
-            precioKilo = Math.round(Uruguay.precioFranja1 * 3 + Uruguay.precioFranja2 * 2 + (pesoAbonadoCliente - 5) * Uruguay.precioFranja3);
-        }
-    }
+    let pesoAbonadoCliente = Number($("#txtPayByClient").val());
+    let pesoRealProducto = Number($("#txtRealW").val());
 
-    return precioKilo;
-}
+    let peso = abonadoPorCliente ? pesoAbonadoCliente : pesoRealProducto
 
-function calcularPrecioRealDelProducto(pesoRealProducto,paisSeleccionado){
-    let precioKilo;
-    pesoAbonadoCliente = Number($("#txtPayByClient").val());
-    pesoRealProducto = Number($("#txtRealW").val());
-    paisSeleccionado = $("#slcCountrySelector").val();
-    if(paisSeleccionado === "ar"){
-        if(pesoRealProducto > 0 && pesoRealProducto <= 3){
-            precioKilo =  Math.round(pesoRealProducto * Argentina.precioFranja1);
-        }
-        if(pesoRealProducto > 3.01 && pesoRealProducto <= 5){
-            precioKilo = Math.round(Argentina.precioFranja1 * 3 +  (pesoRealProducto - 3) * Argentina.precioFranja2);
-        }
-        if(pesoRealProducto > 5.01){
-            precioKilo = Math.round(Argentina.precioFranja1 * 3 + Argentina.precioFranja2 * 2 + (pesoRealProducto - 5) * Argentina.precioFranja3);
-        }
+    if (pesoAbonadoCliente > 0 && pesoAbonadoCliente <= 3) {
+        precioKilo = Math.round(peso * obtenerFranja(1));
     }
-    if(paisSeleccionado === "ur"){
-        if(pesoRealProducto > 0 && pesoRealProducto <= 3){
-            precioKilo =  Math.round(pesoRealProducto * Uruguay.precioFranja1);
-        }
-        if(pesoRealProducto > 3.01 && pesoRealProducto <= 5){
-            precioKilo = Math.round((Uruguay.precioFranja1 * 3) +  (pesoRealProducto - 3) * Uruguay.precioFranja2);
-        }
-        if(pesoRealProducto > 5.01){
-            precioKilo = Math.round(Uruguay.precioFranja1 * 3 + Uruguay.precioFranja2 * 2 + (pesoRealProducto - 5) * Uruguay.precioFranja3);
-        }
+    if (pesoAbonadoCliente >= 3.01 && peso <= 5) {
+        precioKilo = Math.round(obtenerFranja(1) * 3 + (peso - 3) * obtenerFranja(2));
     }
-
+    if (pesoAbonadoCliente > 5.01) {
+        precioKilo = Math.round(obtenerFranja(1) * 3 + obtenerFranja(2) * 2 + (peso - 5) * obtenerFranja(3));
+    }
     return precioKilo;
 }
 
 $("#btnCalculateWhDifference").click(calculoFinal);
 
-function calculoFinal(){
-    let a =  Number($("#txtPayByClient").val());
-    let b = Number($("#txtRealW").val());
-    let c = $("#slcCountrySelector").val();
-    let d = $("#txtNumeroOrden").val();
-    let tempAbonadoClente = calcularPrecioAbonadoPorCliente(a,b);
-    let tempPesoReal = calcularPrecioRealDelProducto(c,b);
-    let tempFinal;
-    console.log("abonado cliente : " + tempAbonadoClente);
-    console.log("peso real : " + tempPesoReal);
-    tempFinal = tempPesoReal - tempAbonadoClente;
-    $("#pMensajes").html(`<h3>Cliente debe abonar: </h3> ${tempFinal} USD<br>
+function calculoFinal() {
+    let numeroOrden = $("#txtNumeroOrden").val();
+    let precioAbonadoCliente = calcularPrecio(true);
+    let precioPesoReal = calcularPrecio(false);
+    
+    console.log("abonado cliente : " + precioAbonadoCliente);
+    console.log("peso real : " + precioPesoReal);
+    let diferenciaPrecio = precioPesoReal - precioAbonadoCliente;
+
+    $("#pMensajes").html(`<h3>Cliente debe abonar: </h3> ${diferenciaPrecio} USD<br>
     <h3>Link de pago : </h3>
-    https://tiendamia.com/crear-link-ap?oid=${d}&m=${tempFinal}
+    https://tiendamia.com/crear-link-ap?oid=${numeroOrden}&m=${diferenciaPrecio}
     `);
 }
-
-
